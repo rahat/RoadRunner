e4d2cf1651694ed408148c985cd770d204afae3d
Rahat Mahmud <rahat@Rahats-MacBook-Pro.local>
2015-05-13 12:17:36 -0400

    Radcliffe-CameraActivity

--- app/src/main/java/cst/roadrunner/cameraActivity.java
+++ app/src/main/java/cst/roadrunner/cameraActivity.java
@@ -4,18 +4,27 @@ package cst.roadrunner;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareButton;
 import com.facebook.share.widget.ShareDialog;
 
 public class cameraActivity extends Activity {
 
     CallbackManager callbackManager;
     ShareDialog shareDialog;
+    ImageView imgFavorite;
+    Button selfieButton;
+    Bitmap image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +35,44 @@ public class cameraActivity extends Activity {
         callbackManager = CallbackManager.Factory.create();
         shareDialog = new ShareDialog(this);
 
-        if (ShareDialog.canShow(SharePhotoContent.class)) {
-            Bitmap image =...
-            SharePhoto photo = new SharePhoto.Builder()
-                    .setBitmap(image)
-                    .build();
-            SharePhotoContent content = new SharePhotoContent.Builder()
-                    .addPhoto(photo)
-                    .build();
-            shareDialog.show(content);
-        }
+        imgFavorite = (ImageView) findViewById(R.id.imageView1);
+        selfieButton = (Button) findViewById(R.id.selfieButton);
+        selfieButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open();
+            }
+        });
+
+        ShareButton shareButton = (ShareButton)findViewById(R.id.shareButton);
+
+        shareButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ShareDialog.canShow(SharePhotoContent.class)) {
+                    image = ((BitmapDrawable) imgFavorite.getDrawable()).getBitmap();
+                    SharePhoto photo = new SharePhoto.Builder()
+                            .setBitmap(image)
+                            .build();
+                    SharePhotoContent content = new SharePhotoContent.Builder()
+                            .addPhoto(photo)
+                            .build();
+                    shareDialog.show(content);
+                }
+            }
+        });
+    }
+
+    public void open() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
     }
 
     @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        imgFavorite.setImageBitmap(bp);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
