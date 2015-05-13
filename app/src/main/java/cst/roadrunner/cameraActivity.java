7ab3d10f5785160220261dffdc95229e0011ff78
Rahat Mahmud <rahatmahmud@students.ptechnyc.org>
2015-05-12 23:38:09 -0400

    Code to upload photo to Facebook

--- app/src/main/java/cst/roadrunner/cameraActivity.java
+++ app/src/main/java/cst/roadrunner/cameraActivity.java
@@ -1,5 +1,46 @@
 package cst.roadrunner;
 
 
-public class cameraActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
+
+public class cameraActivity extends Activity {
+
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+
+        if (ShareDialog.canShow(SharePhotoContent.class)) {
+            Bitmap image =...
+            SharePhoto photo = new SharePhoto.Builder()
+                    .setBitmap(image)
+                    .build();
+            SharePhotoContent content = new SharePhotoContent.Builder()
+                    .addPhoto(photo)
+                    .build();
+            shareDialog.show(content);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
